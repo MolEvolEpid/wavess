97cc62fdbb085bf4330df9e274e475977c34e337
Zena Lapp <zenalapp@gmail.com>
2024-09-30 11:23:42 -0600

    Update rate_to_probability

--- R/rate_to_probability.R
+++ R/rate_to_probability.R
@@ -1,23 +1,22 @@
-#' Convert a rate to a Poisson probability
+#' Convert a rate to a probability
 #'
 #' The `wavess()` function takes probabilities,
 #' but sometimes in the literature you find rates instead.
-#' This funciton allows you to convert that rate to a probability.
+#' This function allows you to convert that rate to a probability.
 #'
 #' @param rate Rate to be converted
-#' @param k Number of events observed in the rate-defined time period (default: 1)
+#' @param time Time period (default: 1, e.g. 1 generation)
 #'
 #' @return
-#' Probability that the event occurs in the rate-defined time period
+#' Probability that at least one event occurs in the time period
 #' @export
 #'
 #' @examples
 #' rate_to_probability(2.1)
 #' rate_to_probability(0.1)
 #' rate_to_probability(3.5e-5)
-rate_to_probability <- function(rate, k = 1){
+rate_to_probability <- function(rate, time = 1){
   check_is_numeric(rate, 'rate')
-  check_is_numeric(k, 'rate')
-  if(k < 0) check_is_pos(k, 'k')
-  return(rate^k*exp(-rate)/factorial(k))
+  check_is_pos(time, 'rate')
+  return(1-exp(-rate*time))
 }
