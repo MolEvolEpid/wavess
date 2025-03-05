ad58f22be9239e53c546d5cb749e3ee4ee68071a
Zena Lapp <zenalapp@gmail.com>
2025-03-05 13:46:12 -0700

    Add summary stats WIP

--- R/calc_tr_stats.R
+++ R/calc_tr_stats.R
@@ -32,30 +32,76 @@ calc_tr_stats <- function(tr, timepoints) {
     )
   }
 
-  transitions <- phangorn::parsimony(tr, phangorn::phyDat(factor(timepoints), type = "USER"))
+  transitions <- phangorn::parsimony(tr, phangorn::phyDat(factor(timepoints), type = "USER"))/dplyr::n_distinct(timepoints)
 
   rtt_ttt <- calc_tr_dists(tr)
   diverg_divers <- lapply(unique(timepoints), function(x) {
-    calc_tr_dists(tr, names(timepoints)[timepoints == x])
+    calc_tr_dists(tr, names(timepoints)[timepoints == x]) |>
+      dplyr::mutate(timepoint = x, .before = 1)
   }) |>
-    dplyr::bind_rows() |>
+    dplyr::bind_rows()  |>
     dplyr::summarize(
       mean_divergence = mean(mean_rtt, na.rm = TRUE),
-      mean_diversity = mean(mean_ttt, na.rm = TRUE)
+      mean_diversity = mean(mean_ttt, na.rm = TRUE),
+      var_divergence = var(mean_rtt, na.rm = TRUE),
+      var_diversity = var(mean_ttt, na.rm = TRUE)
     ) |>
     unlist() |>
     unname()
 
+
+  dists <- lapply(unique(timepoints), function(y) {
+    tips <- names(timepoints)[timepoints == y]
+    ntip <- length(tr$tip.label)
+    root_node <- ntip + 1
+    d <- ape::dist.nodes(tr)
+    colnames(d) <- rownames(d) <- c(tr$tip.label, paste0("node", root_node:(ntip + ape::Nnode(tr))))
+    # root-to-tip distance
+    rtt <- d[tips, root_node] |>
+      tibble::enframe() |>
+      dplyr::mutate(stat_name = 'root_to_tip')
+    # tip-to-tip distance
+    d <- d[tips, tips]
+    ttt <- d[upper.tri(d)] |>
+      tibble::enframe() |>
+      dplyr::mutate(stat_name = 'tip_to_tip') |>
+      dplyr::mutate(name = as.character(name))
+    dplyr::bind_rows(rtt, ttt) |>
+      dplyr::mutate(timepoint = y, .before = 1)
+  }) |>
+    dplyr::bind_rows() |>
+    dplyr::mutate(timepoint = as.numeric(as.character(timepoint)))
+
+  slopes <- bind_rows(lm(value ~ timepoint, dists |>
+                 filter(stat_name == 'root_to_tip')) |>
+              coef() |>
+              tibble::enframe() |>
+              dplyr::mutate(type = 'root_to_tip'),
+            lm(value ~ timepoint, dists |>
+                 dplyr::filter(stat_name == 'tip_to_tip')) |>
+              coef() |>
+              tibble::enframe() |>
+              dplyr::mutate(type = 'tip_to_tip')) |>
+    dplyr::filter(name == 'timepoint')
+
   tibble::tibble(
-    stat_name = c("mean_leaf_depth", "mean_int_bl", "mean_ext_bl", "mean_tip_to_tip",
-                  "mean_divergence", "mean_diversity", "transition_score"),
+    stat_name = c("mean_leaf_depth", "mean_bl",
+                  "mean_int_bl", "mean_ext_bl",
+                  "mean_tip_to_tip", "mean_root_to_tip",
+                  "mean_divergence", "mean_diversity",
+                  "divergence_slope", "diversity_slope",
+                  "transition_score"),
     stat_value = c(
       treebalance::avgLeafDepI(tr), # average leaf depth (normalized sackin)
+      mean(tr$edge.length), # branch lengths
       mean(tr$edge.length[tr$edge[, 2] > ape::Ntip(tr)]), # internal branch lengths
       mean(tr$edge.length[tr$edge[, 2] <= ape::Ntip(tr)]), # external branch lengths
       rtt_ttt$mean_ttt, # tip-to-tip
+      rtt_ttt$mean_rtt, # root-to-tip
       diverg_divers[1], # divergence
       diverg_divers[2], # diversity
+      slopes$value[slopes$type == 'root_to_tip'],
+      slopes$value[slopes$type == 'tip_to_tip'],
       transitions # transition score
     )
   )
@@ -123,10 +169,8 @@ calc_tr_dists <- function(tr, tips = NULL) {
   colnames(d) <- rownames(d) <- c(tr$tip.label, paste0("node", root_node:(ntip + ape::Nnode(tr))))
   # mean root-to-tip distance
   mean_rtt <- mean(d[tips, root_node])
-  median_rtt <- median(d[tips, root_node])
   # mean tip-to-tip distance
   d <- d[tips, tips]
   mean_ttt <- mean(d[upper.tri(d)])
-  median_ttt <- median(d[upper.tri(d)])
-  return(tibble::tibble(mean_rtt, median_rtt, mean_ttt, median_ttt))
+  return(tibble::tibble(mean_rtt, mean_ttt))
 }
