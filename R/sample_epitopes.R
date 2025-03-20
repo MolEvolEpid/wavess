5a707093b415aff4b53d2e87070e559a186353e9
Zena Lapp <zenalapp@gmail.com>
2025-03-20 10:09:13 -0600

    Update documentation

--- R/sample_epitopes.R
+++ R/sample_epitopes.R
@@ -17,7 +17,7 @@
 #' @param aa_epitope_length Amino acid epitope length
 #' @param max_fit_cost Maximum fitness cost of an epitope, must be in the range
 #'   [0,1) where 0 indicates no cost. 1, which indicates no ability to
-#'   survive, is not allowed (default: 0.4)
+#'   survive, is not allowed (default: 0.3)
 #' **note that the model output is very sensitive to this parameter**
 #' @param max_resamples Maximum number of resampling events to attempt; this is
 #'   to prevent an infinite loop (default: 100)
